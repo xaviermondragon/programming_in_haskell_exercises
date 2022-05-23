6fd44b2aff4d25108045712e8b226b17bac68443
xaviermondragon <xavierrmondragon@gmail.com>
2022-05-23 13:24:35 +0200

    Exercises 10.4 qnd 10.5.

--- chapter_10/solutions.hs
+++ chapter_10/solutions.hs
@@ -1,3 +1,7 @@
+import Data.Char
+import System.IO
+
+
 -- Exercise 1
 putStr' :: String -> IO ()
 putStr' xs = sequence_ [putChar x | x <- xs]
@@ -22,3 +26,30 @@ putBoard' xs = auxFunc (zip [1..] xs)
 
 putBoard'' :: Board -> IO ()
 putBoard'' xs = sequence_ [putRow row num | (row,num) <- zip [1..] xs]
+
+
+-- Exercise 4
+getInt :: IO Int
+getInt = do n <- getLine
+            return (read n :: Int)
+                   
+
+
+partial :: Int -> Int -> IO ()
+partial current 0         = putStrLn ("The total is: " ++ show current)
+partial current remaining = do x <- getInt
+                               partial (current + x) (remaining -1)
+
+
+adder :: IO ()
+adder = do putStr "How many numbers?: "
+           n <- getInt
+           partial 0 n
+
+
+-- Exercise 5
+adder' :: IO ()
+adder' = do putStr "How many numbers?: "
+            n <- getInt
+            ints <- sequence [getInt | _ <- [1..n]]
+            putStrLn ("The total is: " ++ show (sum ints))
